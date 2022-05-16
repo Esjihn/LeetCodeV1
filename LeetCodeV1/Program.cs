41810ff35fd64e1df3713b5abf440065ddb106ca
Esjihn <sysnom@gmail.com>
2022-05-16 07:13:03 -0400

    Begin IsValid BruteForce

--- LeetCodeV1/Program.cs
+++ LeetCodeV1/Program.cs
@@ -4,10 +4,45 @@
     {
         public static void Main(string[] args)
         {
-            foreach (int item in TwoSum(new int[] { 3, 3 }, 6))
+            string str1 = "()";
+            string str2 = "()[]{}";
+            string str3 = "(]";
+            string str4 = "([)]";
+
+            Console.WriteLine(IsValid(str1));
+            Console.WriteLine(IsValid(str2));
+            Console.WriteLine(IsValid(str3));
+            Console.WriteLine(IsValid(str4));
+        }
+
+        /// <summary>
+        /// Open brackets must be closed by the same type of brackets.
+        /// Open brackets must be closed in the correct order.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool IsValid(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s)) return false;
+
+            // Brute Force
+
+            // Library 
+            Dictionary<int, char> validStrDict = new Dictionary<int, char>();
+            
+            validStrDict.Add(0, '(');
+            validStrDict.Add(1, ')');
+            validStrDict.Add(2, '[');
+            validStrDict.Add(3, ']');
+            validStrDict.Add(4, '{');
+            validStrDict.Add(5, '}');
+
+            for (int i = 0; i < s.Length; i++)
             {
-                Console.Write(item + " ");
+                
             }
+
+            return false;
         }
 
         /// <summary>
